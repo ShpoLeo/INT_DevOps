b922ae0b64ac67c654afed27e126c57439d6fbfe
ShpoLeo <shpolyanskiil@gmail.com>
2024-11-03 18:32:58 +0200

    Delete logs.py

--- logs.py
+++
@@ -1,8 +0,0 @@
-import logging
-import sys
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(levelname)s:%(filename)s:%(lineno)d,%(funcName)s - %(message)s',
-                    handlers=[logging.FileHandler("system_info.log"),
-                              logging.StreamHandler()])
-
-logger = logging.getLogger(__file__)
